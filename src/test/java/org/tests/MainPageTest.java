11887b7d3b54b38b621a570bd4b122ec8ea6038d
aleksandr.brynza <aleksandr.brynza@simbirsoft.com>
2025-12-15 10:25:24 +0400

    Задача U1. Оставшиеся тест кейсы

--- src/test/java/org/tests/MainPageTest.java
+++ src/test/java/org/tests/MainPageTest.java
@@ -28,6 +28,21 @@ public class MainPageTest extends BaseTest {
         System.out.println("Футер отображается: " + isFooterDisplayed);
         Assert.assertTrue(isFooterDisplayed, "Футер должен отображаться");
 
+        boolean isSliderWorks = mainPage.isSliderWorks();
+        System.out.println("Слайдер меняется: " + isSliderWorks);
+        Assert.assertTrue(isSliderWorks, "Слайдер должен меняться при свайпе");
+
+        boolean isSliderWorksBackwards = mainPage.isSliderWorksBackwards();
+        System.out.println("Слайдер возвращается назад: " + isSliderWorksBackwards);
+        Assert.assertTrue(isSliderWorksBackwards, "Слайдер должен возвращаться назад при свайпе туда-обратно");
+
+        boolean isHeaderScrolledToFooterVisible = mainPage.isHeaderScrolledToFooterVisible();
+        System.out.println("Главное меню видно при скроллинге в конец страницы: " + isHeaderScrolledToFooterVisible);
+        Assert.assertTrue(isHeaderScrolledToFooterVisible, "Главное меню должно быть видно при скроллинге в конец страницы");
+
+        boolean isHeaderScrolledToFixCoordinateVisible = mainPage.isHeaderScrolledToFixCoordinateVisible();
+        System.out.println("Главное меню видно при скроллинге: " + isHeaderScrolledToFixCoordinateVisible);
+        Assert.assertTrue(isHeaderScrolledToFixCoordinateVisible, "Главное меню должно быть видно при скроллинге");
     }
 
 }
